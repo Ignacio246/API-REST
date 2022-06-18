a2edb0f95f6160315276b4bd6082efe95558894b
Ignacio246 <1718110989@utectulancingo.edu.mx>
2022-06-18 04:16:14 +0000

    Endpoints finished

--- code/main.py
+++ code/main.py
@@ -19,11 +19,11 @@ class Cliente(BaseModel):
 
 app = FastAPI()
 
-"""
+
 @app.get("/", response_model=Respuesta)
 async def index():
     return {"message": "API-REST"}
-"""
+
 
 
 @app.get("/clientes/", response_model=List[Cliente])
@@ -34,6 +34,7 @@ async def clientes():
         cursor.execute('SELECT * FROM clientes')
         response = cursor.fetchall()
         return response
+
 
 """
 @app.get("/clientes/", response_model=List[Cliente])
@@ -45,6 +46,7 @@ async def clientes(offset:int=0,limit:int=10):
         response = cursor.fetchall()
         return response
 """
+
 @app.get("/clientes/{id_clientes}", response_model=Cliente)
 async def clientes():
     with sqlite3.connect('sql/clientes.sqlite') as connection:
@@ -53,12 +55,25 @@ async def clientes():
         cursor.execute('SELECT * FROM clientes')
         response = cursor.fetchone()
         return response
-
-
+"""
 @app.post("/clientes/", response_model=Respuesta)
 async def post_cliente(cliente=Cliente):
     with sqlite3.connect('sql/clientes.sqlite') as connection:
         connection.row_factory = sqlite3.Row
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO clientes (id_clientes,nombre,email VALUES ()"
-        (cliente.id_clientes,cliente.nombre,cliente.email))
+        cursor.execute("INSERT INTO clientes (nombre,email VALUES ('ignacio','nacho@email.com')")
+        connection.commit()
+        connection.close()
+"""
+
+@app.post("/clientes/")
+def post_cliente(nombre: str, email:str):
+    return f"Cliente {nombre} {email} guardado"
+
+@app.put("/clientes/")
+def put_cliente(nombre: str, email:str):
+    return f"Cliente {nombre} {email} actualizado"
+
+@app.delete("/clientes/{id_clientes}")
+def delete_clientes(id_clientes: int):
+    return f"Cliente {id_clientes} eliminado"
